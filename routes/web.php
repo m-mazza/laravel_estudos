404f04d71a62b2151594a8780f33c218cd2b0d18
Marcus Mazza <m-mazza@live.com>
2023-12-24 23:13:23 -0300

    estudos laravel 24/12/2023

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use illumiante\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -31,6 +32,69 @@ Route::get('/teste/{nome?}', function($nome=null) {
         echo "Olá $nome. Seja bem-vindo";
     }
     else {
-        echo 'não há nome setado.';
+        echo 'Você não digitou nenhum nome.';
     }
+});
+
+//parâmetros com regras
+//a cláusula 'where' permite que, por meio de expressões regulares, determine as regras dos parâmetros
+Route::get('/rotacomregras/{nome}/{n}', function($nome, $n) {
+    for ($i=0; $i < $n; $i++) {
+        echo "Olá, Seja bem vindo, $nome <br>";
+    }
+})->where('nome','[A-Za-z]+')->where('n','[0-9]+');
+
+//agrupamento de rodas
+//para agrupamento de rotas é necessário trocar o método get por 'prefix'
+//o agrupamento ocorre por meio da função group
+Route::prefix('/app')->group(function() {
+    Route::get('/', function(){
+        return  view('app');
+    });
+    Route::get('/user', function(){
+        return view('user');
+    });
+    Route::get('/profile', function(){
+        return view('profile');
+    });
+});
+
+//nomeando rotas
+//A nomeação de rotas permite que não trabalhe com constantes no sistema.
+Route::prefix('/aplication')->group(function() {
+    Route::get('/', function(){
+        return  view('app');
+    })->name('app');
+
+    Route::get('/user', function(){
+        return view('user');
+    })->name('app.user');
+
+    Route::get('/profile', function(){
+        return view('profile');
+    })->name('app.profile');
+});
+
+//A renomeação de rotas não é necessariamente utilizanda dentro de um grupo, como está acima
+Route::get('/produtos', function() {
+    echo '<h1>Produtos</h1>';
+    echo '<ol>';
+    echo '<li>Notebook</li>';
+    echo '<li>Mouse</li>';
+    echo '<li>Keyboard</li>';
+    echo '</ol>';
+})->name('meusprodutos');
+
+//redirecionamento de rota
+Route::redirect('todososprodutos1', 'produtos', 301); //primeira forma -> usando o Route
+
+//essa é a segunda forma de fazer redirecionamento
+Route::get('todososprodutos2', function () {
+    return redirect()->route('meusprodutos');
+});
+
+//////////////////////////////////////////////////////////
+
+Route::post('requisicoes', function (Request $request) {
+    return 'Olá Planeta';
 });
